df1c5a76fcd39d3e7749e29d2742b3cadcf79577
Ruiqi Xing <eva.rq.xing@gmail.com>
2020-03-01 20:44:56 +0800

    Updates

---
+++ precache-manifest.54d7b6f11c5964c01890ee2eb1eb8b25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d7fad8d4934e8d3427bc763175178e",
+    "url": "/color-recognition-brain/index.html"
+  },
+  {
+    "revision": "d96d4136de0cc6000a0e",
+    "url": "/color-recognition-brain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "2bb665606a3035b04b77",
+    "url": "/color-recognition-brain/static/css/main.61cadf1b.chunk.css"
+  },
+  {
+    "revision": "d96d4136de0cc6000a0e",
+    "url": "/color-recognition-brain/static/js/2.a461aafa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/color-recognition-brain/static/js/2.a461aafa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bb665606a3035b04b77",
+    "url": "/color-recognition-brain/static/js/main.277de252.chunk.js"
+  },
+  {
+    "revision": "64c3dcd62a38f4f4c017",
+    "url": "/color-recognition-brain/static/js/runtime-main.64c1cd5d.js"
+  },
+  {
+    "revision": "8580a503df9783ecde1fa075b75ccc89",
+    "url": "/color-recognition-brain/static/media/brain.8580a503.png"
+  }
+]);
