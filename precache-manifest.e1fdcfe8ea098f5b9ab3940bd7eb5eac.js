66661bcefd87ebd3c6bf92bd7bb3c4331f594d08
Ruiqi Xing <eva.rq.xing@gmail.com>
2020-04-02 21:20:31 +0800

    Updates

--- precache-manifest.e1fdcfe8ea098f5b9ab3940bd7eb5eac.js
+++ precache-manifest.e1fdcfe8ea098f5b9ab3940bd7eb5eac.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0884950fdac0de6fa8923e40dc85a6c3",
+    "revision": "f35296c2c3ba4b9d590eabe2c9506478",
     "url": "/color-recognition-brain/index.html"
   },
   {
-    "revision": "70f55a6965eb1a2c5eac",
+    "revision": "53072e412e40bac288d1",
     "url": "/color-recognition-brain/static/css/2.eb45ad44.chunk.css"
   },
   {
-    "revision": "1187e22e9b388f4bf490",
-    "url": "/color-recognition-brain/static/css/main.1d59d758.chunk.css"
+    "revision": "09f6b59b97d539ba4ab5",
+    "url": "/color-recognition-brain/static/css/main.6142981b.chunk.css"
   },
   {
-    "revision": "70f55a6965eb1a2c5eac",
-    "url": "/color-recognition-brain/static/js/2.f6261f8b.chunk.js"
+    "revision": "53072e412e40bac288d1",
+    "url": "/color-recognition-brain/static/js/2.f04d0b74.chunk.js"
   },
   {
     "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
-    "url": "/color-recognition-brain/static/js/2.f6261f8b.chunk.js.LICENSE.txt"
+    "url": "/color-recognition-brain/static/js/2.f04d0b74.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1187e22e9b388f4bf490",
-    "url": "/color-recognition-brain/static/js/main.e64f27a6.chunk.js"
+    "revision": "09f6b59b97d539ba4ab5",
+    "url": "/color-recognition-brain/static/js/main.627ac4fa.chunk.js"
   },
   {
     "revision": "64c3dcd62a38f4f4c017",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/color-recognition-brain/precache-manifest.2156192eb6563178a7e1f60fe70dc424.js"
+  "/color-recognition-brain/precache-manifest.e1fdcfe8ea098f5b9ab3940bd7eb5eac.js"
 );
 
 self.addEventListener('message', (event) => {
